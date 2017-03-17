00b4fab70a96ddd335d84997425bf7f707eb30e3
max19736 <user@2-5>
2017-03-17 12:21:33 +0800

    updata

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Class1
+    {
+        public string date { get; set; }
+        public string number { get; set; }
+        public string name { get; set; }
+        public string phonenumber { get; set; }
+        public string URL { get; set; }
+        public string Email { get; set; }
+        public string address { get; set; }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = FindClass1 ();
+        }
+
+        public static List<Class1> FindClass1()
+        {
+            List<Class1> classes = new List<Class1>();
+            var xml =XElement.Load( @"D:\max19736\max19736.xml");
+
+            var classesNode = xml.Descendants("row_item").ToList();
+
+            for(var i=0;i< classesNode.Count(); i++)
+            {
+                var classNode = classesNode[i];
+            }
+            classesNode
+                .Where(x => !x.IsEmpty).ToList()
+                .ForEach(classNode =>
+                {
+                    var date = classNode.Element("登記證核准日期").Value.Trim();
+
+
+                });
+
+            return classes;
+        }
+    }
+}
